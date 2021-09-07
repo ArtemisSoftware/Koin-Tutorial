5009aa460d1bfd270f3731b10b480dda17c1b4ed
gustavo <gustavo.isaac@gmail.com>
2021-09-07 15:15:09 +0100

    Layout item_tv_show

--- app/src/main/java/com/artemissoftware/kointutorial/ui/MainActivity.kt
+++ app/src/main/java/com/artemissoftware/kointutorial/ui/MainActivity.kt
@@ -4,9 +4,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.artemissoftware.kointutorial.R
 
+import org.koin.android.viewmodel.ext.android.viewModel
+
 class MainActivity : AppCompatActivity() {
+
+    private val tvShowViewModel : TvShowViewModel by viewModel()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        tvShowViewModel.responseTvShow.observe(this, { listTvShows ->
+
+
+
+        })
     }
 }
